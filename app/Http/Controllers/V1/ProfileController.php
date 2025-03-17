e1dea7bf113e930e894ad1834b021a799dd3c427
Yahya afadisse <yahyaafadisse92@gmail.com>
2025-03-17 22:20:37 +0000

    separer request to profile Request

--- app/Http/Controllers/V1/ProfileController.php
+++ app/Http/Controllers/V1/ProfileController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers\V1;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\ProfileRequest;
 use Illuminate\Support\Facades\Storage;
-use App\Http\Controllers\Controller;
 
 class ProfileController extends Controller
 {
@@ -17,16 +18,10 @@ class ProfileController extends Controller
         ]);
     }
 
-    public function update(Request $request)
+    public function update(ProfileRequest $request)
     {
         $user = Auth::user();
-
-        $validatedData = $request->validate([
-            'name' => 'sometimes|string|max:255',
-            'email' => 'sometimes|string|email|max:255|unique:users,email,' . $user->id,
-            'password' => 'sometimes|string|min:8|confirmed',
-            'photo' => 'sometimes|image|mimes:jpeg,png,jpg,gif|max:2048'
-        ]);
+        $validatedData = $request->validated();
 
         if (isset($validatedData['password'])) {
             $validatedData['password'] = Hash::make($validatedData['password']);
---
+++ app/Http/Requests/ProfileRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'sometimes|string|max:255',
+            'email' => 'sometimes|string|email|max:255|unique:users,email,' . $this->user()->id,
+            'password' => 'sometimes|string|min:8|confirmed',
+            'photo' => 'sometimes|image|mimes:jpeg,png,jpg,gif|max:2048'
+        ];
+    }
+}
