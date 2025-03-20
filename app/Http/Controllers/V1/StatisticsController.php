4e8f2a62bb8d864024fddcd8a1653db9ed6abcf2
Yahya afadisse <yahyaafadisse92@gmail.com>
2025-03-20 00:18:43 +0000

    appliquer permissions sur les statistiques

--- app/Http/Controllers/V1/StatisticsController.php
+++ app/Http/Controllers/V1/StatisticsController.php
@@ -5,14 +5,19 @@ namespace App\Http\Controllers\V1;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
 use App\Services\StatisticsService;
+use Illuminate\Routing\Controller as BaseController;
 
-class StatisticsController extends Controller
+class StatisticsController extends BaseController
 {
     protected $statisticsService;
 
     public function __construct(StatisticsService $statisticsService)
     {
         $this->statisticsService = $statisticsService;
+
+        $this->middleware('can:getCoursesStats')->only(['getCoursesStats']);
+        $this->middleware('can:getCategoriesStats')->only(['getCategoriesStats']);
+        $this->middleware('can:getTagsStats')->only(['getTagsStats']);
     }
 
     public function getCoursesStats(): JsonResponse
